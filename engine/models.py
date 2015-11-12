7de55af1828759e47930a7155f898b3fc756200f
Aadil Bhatti <aadilzbhatti@gmail.com>
2015-11-11 18:15:20 -0600

    added str

--- engine/models.py
+++ engine/models.py
@@ -5,6 +5,12 @@ class Recipe(models.Model):
     image_url = models.TextField(max_length=2000)
     recipe_url = models.TextField(max_length=2000, unique=True)
 
+    def __str__(self):
+    	return self.title
+
 class Ingredients(models.Model):
     title = models.CharField(max_length=30, unique=True)
     recipe = models.ForeignKey(Recipe)
+
+    def __str__(self):
+    	return self.title
