9087bb9bdc79800e315efdac49486942b766f241
ssapra2 <ssapra2@illinois.edu>
2015-11-14 12:25:37 -0600

    elasticsearch theoretically works

---
+++ engine/search_indexes.py
@@ -0,0 +1,22 @@
+import datetime
+from haystack import indexes
+from models import Recipe, Ingredients
+
+class RecipeIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    def get_model(self):
+        return Recipe
+
+    # def index_queryset(self, using=None):
+    #     """Used when the entire index for model is updated."""
+    #     return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
+
+class IngredientIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    recipe = indexes.CharField(model_attr='recipe')
+    def get_model(self):
+        return Ingredients
+
+    # def index_queryset(self, using=None):
+    #     """Used when the entire index for model is updated."""
+    #     return self.get_model().objects.filter(pub_date__lte=datetime.datetime.now())
