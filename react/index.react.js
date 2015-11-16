abcdd2cd70840a722ecdc707a3c8ee2f6846537d
ssapra2 <ssapra2@illinois.edu>
2015-11-15 21:55:18 -0600

    and we got data

--- react/index.react.js
+++ react/index.react.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var Search = require('./components/search.react');
 var Loader = require('./components/loading.react');
+var $ = require('jquery');
 var Index = React.createClass({
   getDefaultProps: function(){
     return {
@@ -9,8 +10,7 @@ var Index = React.createClass({
   },
   getInitialState: function(){
     return {
-      'results': [],
-      'load': false
+      'results': []
     };
   },
   searchSubmitted: function(query){
@@ -18,8 +18,13 @@ var Index = React.createClass({
     this.props.load = true;
     this.props.afterSearch = true;
     this.forceUpdate();
-
+    console.log('http://localhost:8000/recipes?q='+query);
     // Call search
+    $.get('http://localhost:8000/recipes?q='+query, function(recipes){
+      this.setState({
+        results: recipes
+      });
+    }.bind(this));
   },
   componentWillUpdate: function(nextProps, nextState){
     this.props.load = false;
@@ -39,10 +44,26 @@ var Index = React.createClass({
           <Search onChange={this.searchSubmitted} search_title={search_title} placeholder={placeholder} disabled={true}/>
         </div>
       );
-    } else {
+    } else if (this.state.results.recipes != null && this.state.results.recipes.length > 0){
       return (
         <div className="row-fluid">
-          Hello World From React.JS
+          Results
+          <Search onChange={this.searchSubmitted} search_title={search_title} placeholder={placeholder} disabled={false}/>
+          <ul>
+            {this.state.results.recipes.map(function(recipe){
+              return(
+                <li>
+                  <img src={recipe.image_url} width="25" height="25"></img>
+                  <p>{recipe.title}</p>
+                </li>
+              );
+            }, this)}
+          </ul>
+        </div>
+      );
+    } else {
+      return(
+        <div className="row-fluid">
           <Search onChange={this.searchSubmitted} search_title={search_title} placeholder={placeholder} disabled={false}/>
         </div>
       );
